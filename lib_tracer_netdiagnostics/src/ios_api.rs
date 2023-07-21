ddcf6e6dc2461a60ba866164cb067e40edf53e48
homework <>
2023-07-21 15:11:59 +0800

    feat:ios api done

--- lib_tracer_netdiagnostics/src/ios_api.rs
+++ lib_tracer_netdiagnostics/src/ios_api.rs
@@ -1,24 +1,21 @@
-#[cfg(any(target_os = "ios", debug_assertions))]
+#[cfg(any(target_os = "ios"))]
 pub mod ios {
-    use std::ffi::{c_char, CStr};
-
+    use std::ffi::{c_char, CStr, CString};
     pub fn init_logger() {
-        // OsLogger::new("com.example.test")
-        //     .level_filter(LevelFilter::Debug)
-        //     .category_level_filter("Settings", LevelFilter::Trace)
-        //     .init()
-        //     .unwrap();
+        log::set_max_level(log::LevelFilter::Info);
     }
 
     #[no_mangle]
-    pub extern "C" fn init() {
+    pub extern "C" fn r_netdiag_init() {
+        println!("test rnet_diginit");
         init_logger();
-        info!("rust oslogger init suc!")
+        info!("rust oslogger init suc!");
     }
 
     #[no_mangle]
-    pub unsafe extern "C" fn ping(ip: *const c_char) {
-        let rt = tokio::runtime::Builder::new_multi_thread()
+    pub unsafe extern "C" fn r_netdiag_ping(ip: *const c_char) -> *const c_char{
+        println!("call ping action ...");
+        let rt: Result<tokio::runtime::Runtime, &str> = tokio::runtime::Builder::new_multi_thread()
             .worker_threads(1)
             .enable_all()
             .build()
@@ -32,5 +29,12 @@ pub mod ios {
             let result = crate::ping_allhost(vec![c_str.to_str().unwrap_or_default()], None).await;
             result
         });
+        let rel_out = match _ping_cli {
+            Ok(r) => String::from(r.join("\n")),
+            Err(e) => e.to_string(),
+        };
+        println!("call ping action ret:{:?}", rel_out);
+        let cs_r =  CString::new(rel_out).unwrap_or_default().into_raw();
+        cs_r
     }
 }
--- lib_tracer_netdiagnostics/src/lib.rs
+++ lib_tracer_netdiagnostics/src/lib.rs
@@ -3,7 +3,7 @@ extern crate log;
 
 pub mod android_api;
 pub mod ios_api;
-use log::{error, info};
+use log::{error};
 use std::net::IpAddr;
 use std::{io, time::Duration};
 use surge_ping::{Client, IcmpPacket, PingIdentifier, PingSequence};
@@ -19,7 +19,7 @@ pub enum NetDiagError {
     #[error("ping bind loc failed2!")]
     Bind2Error(#[from] io::Error),
 
-    #[error("dnt failed!")]
+    #[error("dns failed!")]
     ResolveError(#[from] trust_dns_resolver::error::ResolveError),
 }
 
@@ -41,7 +41,9 @@ type NetDiaglistResult = Result<Vec<String>, NetDiagError>;
 /// ```
 async fn dns_ip(domain_addr: &str) -> Result<Option<String>, NetDiagError> {
     // Construct a new Resolver with default configuration options
+    trace!("dns domain <{}> start...", domain_addr);
     let resolver = TokioAsyncResolver::tokio(ResolverConfig::default(), ResolverOpts::default())?;
+    trace!("resolver suc!");
     let ip = resolver.lookup_ip(domain_addr).await?;
     let ip_str = ip.iter().next().and_then(|f| Some(f.to_string()));
     Ok(ip_str)
@@ -49,18 +51,18 @@ async fn dns_ip(domain_addr: &str) -> Result<Option<String>, NetDiagError> {
 
 // Ping an address $ping_tims times， and return output message（interval 1s）
 async fn ping(client: Client, addr: IpAddr, ping_times: u16) -> NetDiagResult {
-    trace!("ping start here");
+    info!("ping <{}> start...", addr.to_string());
     let mut ping_ret = String::new();
-    let payload = [0; 56];
+    let payload: [u8; 56] = [0; 56];
     let mut pinger = client.pinger(addr, PingIdentifier(rand::random())).await;
-    pinger.timeout(Duration::from_secs(1));
+    pinger.timeout(Duration::from_secs(2));
     let mut interval = tokio::time::interval(Duration::from_secs(1));
     for idx in 0..ping_times {
         interval.tick().await;
         match pinger.ping(PingSequence(idx), &payload).await {
             Ok((IcmpPacket::V4(packet), dur)) => {
                 let r = format!(
-                    "No.{}: {} bytes from {}: icmp_seq={} ttl={:?} time={:0.2?}",
+                    "No.{}: {} bytes from {}: icmp_seq={} ttl={:?} time={:0.2?}\n",
                     idx,
                     packet.get_size(),
                     packet.get_source(),
@@ -74,7 +76,7 @@ async fn ping(client: Client, addr: IpAddr, ping_times: u16) -> NetDiagResult {
 
             Ok((IcmpPacket::V6(packet), dur)) => {
                 let r = format!(
-                    "No.{}: {} bytes from {}: icmp_seq={} hlim={} time={:0.2?}",
+                    "No.{}: {} bytes from {}: icmp_seq={} hlim={} time={:0.2?}\n",
                     idx,
                     packet.get_size(),
                     packet.get_source(),
@@ -86,7 +88,7 @@ async fn ping(client: Client, addr: IpAddr, ping_times: u16) -> NetDiagResult {
                 ping_ret += &r;
             }
             Err(e) => {
-                let r = format!("No.{}: host ping raise error={}", idx, e.to_string());
+                let r: String = format!("idx = {}  error:{}\n", idx, e.to_string());
                 log::debug!("surge-error value is :{:?}", r);
                 ping_ret += &r;
             }
@@ -119,7 +121,7 @@ pub async fn ping_allhost(host_list: Vec<&str>, ping_tims: Option<u16>) -> NetDi
             .build(),
     )?;
     let mut tasks = Vec::new();
-    info!("test task start...");
+    trace!("test task start...");
     for h in host_list {
         match h.parse() {
             Ok(IpAddr::V4(h)) => {
@@ -135,7 +137,7 @@ pub async fn ping_allhost(host_list: Vec<&str>, ping_tims: Option<u16>) -> NetDi
                 //try as dns like to get ip first.
                 let ip = dns_ip(h).await?;
                 if let Some(ip_str) = ip {
-                    info!("lookup is value is {:?}", ip_str);
+                    trace!("lookup is value is {:?}", ip_str);
                     tasks.push(ping(
                         v4_cli.clone(),
                         ip_str.parse().expect("SYNTACTIC-SUGAR"),
