646c45c0f2da3223f1691390f19788d63a4ffe33
dwb <dwb@dwb.ren>
2023-09-22 18:15:24 +0800

    fix: build script

--- lib_tracer_netdiagnostics/gen_examplelib.py
+++ lib_tracer_netdiagnostics/gen_examplelib.py
@@ -4,34 +4,41 @@ import sys
 import shutil
 
 
-def gen_and_copy2_demo(target_os: str = "android", target_arch="aarch64-linux-android"):
+def run(target_os: str = "android", target_arch="aarch64-linux-android"):
     cur_dir = os.path.curdir
 
 
-    def _get_output_path(armv, TARGET_LIB_NAME):
+    def _rust_build(target_name: str):
+        TARGET_LIB_NAME = target_name.startswith("ios") and "libtracer_netdiagnostics.a" or "libtracer_netdiagnostics.so"
+        cmd_str = "cargo build --target {arch} --release".format(arch=target_name)
+        print("run cmd->", cmd_str)
+        b_ret = os.system(cmd_str)
+        if b_ret != 0:
+            sys.exit(1)
+        f_lib = os.path.join(cur_dir, "target", target_arch,
+                                 "release", TARGET_LIB_NAME)        
+        return f_lib
+
+    def _get_output_path(armv, file_name):
         return os.path.join(cur_dir, os.pardir, "build", "android",
-                                 "app", "libs", armv, TARGET_LIB_NAME)
+                                 "app", "libs", armv, file_name)
 
     def _android():
-        TARGET_LIB_NAME = "libtracer_netdiagnostics.so"
         BUILD_TAR = [
             ("arm64-v8a", "aarch64-linux-android"),
             ("armeabi-v7a", "armv7-linux-androideabi"),
         ]
         ret = []
         for (armv, target_name) in BUILD_TAR:
-            cmd_str = "cargo build --target {arch} --release".format(arch=target_name)
-            print("run cmd->", cmd_str)
-            b_ret = os.system(cmd_str)
-            if b_ret != 0:
-                sys.exit(1)
-            f_lib = os.path.join(cur_dir, "target", target_arch,
-                                 "release", TARGET_LIB_NAME)
-            t_lib = _get_output_path(armv, TARGET_LIB_NAME)
+            f_lib = _rust_build(target_name)
+            t_lib = _get_output_path(armv, f_lib.split(os.sep)[-1])
             ret.append((f_lib, t_lib))
         return ret
 
     def _ios():
+        BUILD_TAR = [
+            ("ios-aarch64", "aarch64-apple-ios"),
+        ]
         TARGET_LIB_NAME = "libtracer_netdiagnostics.a"
         t_lib_par = "aarch64-apple-ios"
         f_lib = os.path.join(cur_dir, "target", t_lib_par,
@@ -54,25 +61,13 @@ def gen_and_copy2_demo(target_os: str = "android", target_arch="aarch64-linux-an
         print("output:%s"%out[1])
     print("done!")
 
-
-def rustlib_gen(arch: str = "aarch64-linux-android"):
-    print("cargo build...")
-    cmd_str = "cargo build --target {arch} --verbose --release".format(arch=arch)
-    print("run cmd->", cmd_str)
-    b_ret = os.system(cmd_str)
-    if b_ret != 0:
-        sys.exit(1)
-    print("cargo build suc!")
-
-
 if __name__ == "__main__":
     # android default
     if len(sys.argv) == 1:
         sys.argv.append("android")
 
     plat = sys.argv[1]
-    
     if plat == "android":
-        gen_and_copy2_demo()
+        run()
     elif plat == "ios":
-        gen_and_copy2_demo("ios")
+        run("ios")
